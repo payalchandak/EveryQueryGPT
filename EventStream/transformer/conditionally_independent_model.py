026a6f452a66940ba00006a45df012d40276f8ce
Matthew McDermott <mattmcdermott8@gmail.com>
2023-11-28 15:32:10 -0500

    Added pseudocode/notes to transformer

--- EventStream/transformer/conditionally_independent_model.py
+++ EventStream/transformer/conditionally_independent_model.py
@@ -161,7 +161,7 @@ class ConditionallyIndependentGenerativeOutputLayer(GenerativeOutputLayerBase):
         )
 
 
-class CIPPTForGenerativeSequenceModeling(StructuredGenerationMixin, StructuredTransformerPreTrainedModel):
+class EQTForPretraining(StructuredTransformerPreTrainedModel):
     """The end-to-end model for conditionally independent generative sequence modelling.
 
     This model is a subclass of :class:`~transformers.StructuredTransformerPreTrainedModel` and is designed
@@ -190,71 +190,20 @@ class CIPPTForGenerativeSequenceModeling(StructuredGenerationMixin, StructuredTr
             raise ValueError(f"{config.structured_event_processing_mode} invalid!")
 
         self.encoder = ConditionallyIndependentPointProcessTransformer(config)
+        self.query_encoder = ???(config)
         self.output_layer = ConditionallyIndependentGenerativeOutputLayer(config)
 
         # Initialize weights and apply final processing
         self.post_init()
 
-    def prepare_inputs_for_generation(
-        self, batch: PytorchBatch, past: tuple | None = None, **kwargs
-    ) -> dict[str, Any]:
-        """Returns model keyword arguments that have been modified for generation purposes.
-
-        Args:
-            batch: The batch of data to be transformed.
-            past: The past state of the model, if any. If specified, it must be a tuple containing the past
-                values over prior layers and heads.
-
-            **kwargs: Additional keyword arguments. If "use_cache" is set in the kwargs to False, then the
-                past state is ignored. If not, then the past state is passed through the model to accelerate
-                generation, if past is not None then the batch is trimmed to the last element in the sequence,
-                and the sequential attention mask is pre-computed.
-
-        Raises:
-            ValueError: If the past state is malformed or if there is a dep_graph_el_generation_target in the
-                kwargs that is not None.
-        """
-        # only last sequence element in the batch if past is defined in kwargs
-        batch.time = time_from_deltas(batch)
-
-        use_cache = kwargs.get("use_cache", False)
-        if not use_cache:
-            return {**kwargs, "batch": batch}
-
-        seq_attention_mask = expand_mask(batch.event_mask, batch.time_delta.dtype)
-
-        dep_graph_el_generation_target = kwargs.get("dep_graph_el_generation_target", None)
-        if dep_graph_el_generation_target is not None:
-            raise ValueError(
-                f"Can't use dep_graph_el_generation_target ({dep_graph_el_generation_target}) "
-                "in a conditionally independent model."
-            )
-
-        match past:
-            case None:
-                pass
-
-            case tuple():
-                batch = batch.last_sequence_element_unsqueezed()
-
-            case _:
-                raise ValueError(f"{past} malformed!")
-
-        return {
-            **kwargs,
-            "seq_attention_mask": seq_attention_mask,
-            "batch": batch,
-            "past": past,
-        }
-
     def forward(
-        self, batch: PytorchBatch, is_generation: bool = False, **kwargs
-    ) -> GenerativeSequenceModelOutput:
+        self, batch: PytorchBatch, query_batch: Any, answer: Any, **kwargs
+    ) -> EveryQueryModelOutput:
         """This runs the full forward pass of the model.
 
         Args:
             batch: The batch of data to be transformed.
-            is_generation: Whether or not the model is being used for generation.
+            TODO
             **kwargs: Additional keyword arguments, which are used for output structuring and are forwarded to
                 the encoder. The model specifically looks for use_cache, output_attentions, and
                 output_hidden_states keyword arguments, which control whether additional properties should be
@@ -263,13 +212,13 @@ class CIPPTForGenerativeSequenceModeling(StructuredGenerationMixin, StructuredTr
         Returns:
             The output of the model, which is a `GenerativeSequenceModelOutput` object.
         """
-        use_cache = kwargs.get("use_cache", False)
         output_attentions = kwargs.get("output_attentions", False)
         output_hidden_states = kwargs.get("output_hidden_states", False)
 
         encoded = self.encoder(batch, **kwargs)
+        query_encoded = self.query_encoder(query_batch, **kwargs)
 
-        output = self.output_layer(batch, encoded.last_hidden_state, is_generation=is_generation)
+        output = self.output_layer(encoded.last_hidden_state, query_encoded, answer)
 
         if use_cache:
             output["past_key_values"] = encoded.past_key_values
