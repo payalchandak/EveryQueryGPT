6befa6d9334bbe2f2e541257b0374e411a106b6f
payalchandak <payal.chandak@gmail.com>
2024-02-29 13:54:00 -0500

    shared data embed layer

--- EventStream/transformer/conditionally_independent_model.py
+++ EventStream/transformer/conditionally_independent_model.py
@@ -60,24 +60,6 @@ class EveryQueryOutputLayer(torch.nn.Module):
         
 
 class CIPPTForGenerativeSequenceModeling(StructuredTransformerPreTrainedModel): 
-    """The end-to-end model for conditionally independent generative sequence modelling.
-
-    This model is a subclass of :class:`~transformers.StructuredTransformerPreTrainedModel` and is designed
-    for generative pre-training over "event-stream" data, with inputs in the form of `PytorchBatch` objects.
-    It is trained to solve the generative, multivariate, masked temporal point process problem over the
-    defined measurements in the input data.
-
-    This model largely simply passes the input data through a
-    `ConditionallyIndependentPointProcessTransformer` followed by a
-    `EveryQueryOutputLayer`.
-
-    Args:
-        config: The overall model configuration.
-
-    Raises:
-        ValueError: If the model configuration does not indicate conditionally independent mode.
-    """
-
     def __init__(
         self,
         config: StructuredTransformerConfig,
@@ -88,22 +70,22 @@ class CIPPTForGenerativeSequenceModeling(StructuredTransformerPreTrainedModel):
             raise ValueError(f"{config.structured_event_processing_mode} invalid!")
 
         self.context_encoder = ConditionallyIndependentPointProcessTransformer(config)
-        self.separate_query_embedding_layer = DataEmbeddingLayer(
-            n_total_embeddings=config.vocab_size,
-            out_dim=config.hidden_size,
-            categorical_embedding_dim=config.categorical_embedding_dim,
-            numerical_embedding_dim=config.numerical_embedding_dim,
-            static_embedding_mode=config.static_embedding_mode,
-            split_by_measurement_indices=None,
-            do_normalize_by_measurement_index=config.do_normalize_by_measurement_index,
-            static_weight=config.static_embedding_weight,
-            dynamic_weight=config.dynamic_embedding_weight,
-            categorical_weight=config.categorical_embedding_weight,
-            numerical_weight=config.numerical_embedding_weight,
-        )
-        self.query_embedding_layer = self.separate_query_embedding_layer.query_embedding
-        # uncomment l626 in generative_modelling.py if using separate embedding layers 
-        # self.query_embedding_layer = self.context_encoder.input_layer.data_embedding_layer.query_embedding
+        # self.separate_query_embedding_layer = DataEmbeddingLayer(
+        #     n_total_embeddings=config.vocab_size,
+        #     out_dim=config.hidden_size,
+        #     categorical_embedding_dim=config.categorical_embedding_dim,
+        #     numerical_embedding_dim=config.numerical_embedding_dim,
+        #     static_embedding_mode=config.static_embedding_mode,
+        #     split_by_measurement_indices=None,
+        #     do_normalize_by_measurement_index=config.do_normalize_by_measurement_index,
+        #     static_weight=config.static_embedding_weight,
+        #     dynamic_weight=config.dynamic_embedding_weight,
+        #     categorical_weight=config.categorical_embedding_weight,
+        #     numerical_weight=config.numerical_embedding_weight,
+        # )
+        # self.query_embedding_layer = self.separate_query_embedding_layer.query_embedding
+        # add trainer strategy ddp find unused true in config if using separate embedding layers 
+        self.query_embedding_layer = self.context_encoder.input_layer.data_embedding_layer.query_embedding
         self.query_encoder = None # MLP ?? 
         self.output_layer = EveryQueryOutputLayer(config)
     
