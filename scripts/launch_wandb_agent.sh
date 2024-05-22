0a77ffcd46c6a3324bcceebee378ddd688582de1
payalchandak <payal.chandak@gmail.com>
2024-05-22 11:01:09 -0400

    clean up scripts

--- scripts/launch_wandb_agent.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2002,SC2086,SC2046
-export $(cat .env | xargs)
-
-WANDB_DIR=$PROJECT_DIR/models/.wandb
-
-PYTHONPATH="$EVENT_STREAM_PATH:$PYTHONPATH" wandb agent $ENTITY/$PROJECT_NAME/$1 --count 1 
